b76076a6443adcb3c0cf7d65df5ca64f1698ded3
Jim McKeown <mckeown.james@gmail.com>
2025-02-17 19:17:37 -0500

    basic first template

--- files/views.py
+++ files/views.py
@@ -1,4 +1,8 @@
 from django.http import HttpResponse
-
+from django.shortcuts import render
 def index(request):
     return HttpResponse("Hello, World!")
+
+def files(request):
+    data = [{'name': 'file1', 'size': 100}, {'name': 'file2', 'size': 200}]
+    return render(request, 'files/files.html', {"files": data})
